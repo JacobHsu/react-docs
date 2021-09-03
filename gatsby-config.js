b564edfe90641172f69068650409d0c0c8d14bd1
jacobhsu <jacob.hsu.tw@gmail.com>
2021-09-03 14:51:20 +0800

    ci: fix pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@
 'use strict';
 
 module.exports = {
+  pathPrefix: '/react-docs',
   siteMetadata: {
     title: 'React: A JavaScript library for building user interfaces',
     siteUrl: 'https://reactjs.org',
